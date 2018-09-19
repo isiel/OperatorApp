2d42e4c144510a99a6b593862277742dfb2f9872
Ignacio Siel Brunet <ignacio.siel@mercadolibre.cl>
2018-09-19 18:32:12 -0300

    remove header

--- app/src/main/java/com/mercadolibre/operatorapp/api/ApiInterface.kt
+++ app/src/main/java/com/mercadolibre/operatorapp/api/ApiInterface.kt
@@ -8,7 +8,6 @@ import retrofit2.http.Path
 
 interface ApiInterface {
 
-    //@Headers("X-Auth-Token: 1d38a51a306bbb316a05b8d2442b82cedbe01dabf7a93366d688bbfeeb8fe42f")
     @GET("wms/warehouses/BRTW01/addresses/{address_id}/inventories")
     fun getStock(@Path("address_id") addressId: String): Call<Array<StockItem>>
 
